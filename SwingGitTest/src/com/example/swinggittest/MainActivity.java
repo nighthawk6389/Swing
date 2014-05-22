edcc5cc8ac880a277b7327416ad2454446a4815d
I E <nighthawk6389@yahoo.com>
2014-05-21 22:15:20 -0400

    Init2

--- SwingGitTest/src/com/example/swinggittest/MainActivity.java
+++ SwingGitTest/src/com/example/swinggittest/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); Hello;
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
